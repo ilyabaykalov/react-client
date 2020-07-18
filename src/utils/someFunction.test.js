43c8235bb662aef03c4a7010e7fc749cf5f63a1e
Ilya Baykalov <enot_boris@icloud.com>
2020-07-18 23:50:09 +0700

    файл тестов перемещен

---
+++ src/utils/someFunction.test.js
@@ -0,0 +1,5 @@
+const sum = require('./someFunction');
+
+test('adds 1 + 2', () => {
+  expect(sum(1, 2)).toBe(3);
+});
--- tests/someFunction.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('../src/utils/someFunction');
-
-test('1 + 2 = 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
