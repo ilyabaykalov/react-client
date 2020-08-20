57566b130f1907945702ed10d656560689f10565
Ilya Baykalov <enot_boris@icloud.com>
2020-08-21 01:10:45 +0700

    удален eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -91,9 +91,6 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'eslint-loader',
-          },
-          {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env', '@babel/preset-react'],
